bf3975681cbf0fe0e72c7a007156f2ee6e354e1a
raulsdev <raulsdev@gmail.com>
2021-09-24 15:09:29 -0500

    updated index page

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -15,12 +15,12 @@ export default function AboutMe() {
             className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
             style={{ lineHeight: "3rem" }}
           >
-            {userData.about.title}. Currently working on{" "}
+            {userData.about.title}. Actualmente Trabajando en:{" "}
             <a
               className="bg-red-500 rounded-md px-2 py-1 text-white"
               href={userData.about.currentProjectUrl}
             >
-              {userData.about.currentProject} ✈️
+              {userData.about.currentProject} 💻
             </a>
           </p>
         </div>
